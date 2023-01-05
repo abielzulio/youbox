04bbac5c3c22f99762b0f16165a725f57f867839
Abiel Zulio M <abielzm@gmail.com>
2023-01-05 17:42:34 +0700

    feat: init HomePage

---
+++ types/data.d.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  _id: string
+  id: string
+  product_name: string
+  product_price: string
+  brand: string
+  product_image_url: string
+  product_info: string
+  real_pdp_url: string
+}
