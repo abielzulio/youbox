3a3e9ac04452f8d40654a1a9d4a156c649817b08
Abiel Zulio M <abielzm@gmail.com>
2023-01-06 16:42:05 +0700

    feat: email duplicate handler on signup, add name in params

--- pages/api/sign-up/index.ts
+++ pages/api/sign-up/index.ts
@@ -3,16 +3,21 @@ import dbConnect from "lib/mongoose"
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, password } = req.body
+  const { email, password, name } = req.body
   try {
     dbConnect()
 
-    const user = await User.create({ email, password })
+    const count = await User.countDocuments({ email })
 
-    if (user) return res.status(201).json({ message: "Akun telah dibuat!" })
+    if (count > 0)
+      return res.status(409).json({ message: "E-mail telah digunakan" })
 
-    if (!user)
-      return res.status(500).json({ message: "Terdapat error di server!" })
+    if (count === 0) {
+      const user = await User.create({ email, password, name })
+      if (user) return res.status(200).json({ message: "Akun telah dibuat" })
+      if (!user)
+        return res.status(500).json({ message: "Terdapat error di server" })
+    }
   } catch (error) {
     return res.status(400).json({ message: "Akun tidak dapat dibuat", error })
   }
