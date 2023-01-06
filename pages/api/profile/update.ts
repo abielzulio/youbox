aa9575fe0dbcd1fc7dbf7376cb3af5d1ef99bd66
Abiel Zulio M <abielzm@gmail.com>
2023-01-06 21:19:03 +0700

    feat: update profil api endpoint

---
+++ pages/api/profile/update.ts
@@ -0,0 +1,29 @@
+import User from "model/user"
+import dbConnect from "lib/mongoose"
+import { NextApiRequest, NextApiResponse } from "next"
+import { ObjectId } from "mongodb"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id, email, password, name } = req.body
+  try {
+    dbConnect()
+
+    const update = {
+      $set: {
+        id,
+        email,
+        password,
+        name,
+      },
+    }
+
+    const user = await User.updateOne({ _id: new ObjectId(id) }, update)
+    if (user) return res.status(200).json({ message: "Akun telah diperbarui" })
+    if (!user)
+      return res.status(500).json({ message: "Terdapat error di server" })
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Akun tidak dapat diperbarui", error })
+  }
+}
