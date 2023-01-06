c8bdd79945995237a6575ef6fa5a3aa168a8eae6
Abiel Zulio M <abielzm@gmail.com>
2023-01-06 16:42:37 +0700

    feat: wrong pass and duplicate email handler on login

--- pages/api/log-in/index.ts
+++ pages/api/log-in/index.ts
@@ -7,15 +7,24 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     dbConnect()
 
-    const user = await User.findOne({ email, password })
+    const count = await User.countDocuments({ email })
 
-    if (user)
-      return res.status(200).json({ message: "Akun telah berhasil masuk" })
+    if (count > 1)
+      return res.status(403).json({ message: "Password Anda salah" })
 
-    if (!user)
-      return res
-        .status(500)
-        .json({ message: "Akun tidak tersedia di database!" })
+    if (count === 0)
+      return res.status(500).json({
+        message: "Akun tidak tersedia di database, silahkan buat baru",
+      })
+
+    if (count === 1) {
+      const user = await User.findOne(
+        { email, password },
+        { password: 0, _id: 0, __v: 0 }
+      )
+      if (user) return res.status(200).json(user)
+      if (!user) return res.status(403).json({ message: "Password Anda salah" })
+    }
   } catch (error) {
     return res
       .status(400)
