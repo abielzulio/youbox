bd9415c1bede5a00814fc6a8be453c4a7de93ecc
Abiel Zulio M <abielzm@gmail.com>
2023-01-06 23:45:46 +0700

    feat: serversideprop product individual page

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,34 +1,49 @@
+import EditProduct from "components/product/edit"
+import AuthContext from "context/auth"
+import { GetServerSideProps } from "next"
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useContext, useState } from "react"
 import { Product } from "types/data"
 
-const ProductPage = () => {
+const ProductPage: React.FC<{ product: Product }> = ({ product }) => {
   const router = useRouter()
   const { id } = router.query
-  const [product, setProduct] = useState<Product>()
+  const [localProduct, setProduct] = useState<Product>(product)
+  const [isEditable, setIsEditable] = useState<boolean>(false)
 
-  const fetchProduct = async (id: string | string[] | undefined) => {
-    const res = await fetch(`/api/product/${id}`)
-    const data = await res.json()
-    setProduct(data)
+  const { user } = useContext(AuthContext)
+
+  const editableHandler = (e: any): void => {
+    e.preventDefault()
+    setIsEditable((prev) => !prev)
   }
-
-  useEffect(() => {
-    fetchProduct(id)
-  }, [id])
 
   return (
     <div>
-      {product ? (
-        <>
-          <h1>{product.product_name}</h1>
-          <p>Price: {product.product_price}</p>
-        </>
-      ) : (
-        <p>Loading...</p>
+      {user?.role === "admin" && (
+        <button onClick={editableHandler}>
+          {isEditable ? "selesai edit" : "edit product"}
+        </button>
       )}
+      {isEditable && (
+        <EditProduct product={localProduct} setProduct={setProduct} />
+      )}
+      <h1>{localProduct?.product_name}</h1>
+      <p>Price: {localProduct?.product_price}</p>
+      <p>Stok: {localProduct?.product_info}</p>
     </div>
   )
 }
 
 export default ProductPage
+
+export const getServerSideProps: GetServerSideProps<{
+  product: Product
+}> = async (context) => {
+  const { id } = context.params
+  const res = await fetch(`http://localhost:3000/api/product/${id}`)
+  const product: Product = await res.json()
+  return {
+    props: { product },
+  }
+}
